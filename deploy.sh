ea6543e5f6f2701db77d3429e9db3977cfbe00ab
Tim van Oostrom <tim@depulz.nl>
2021-08-20 22:13:35 +0200

    Clean-up deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,22 +1,16 @@
 #!/usr/bin/env sh
+# taken from https://vitejs.dev/guide/static-deploy.html#github-pages
 
-# abort on errors
 set -e
 
-# build
 npm run build
 
-# navigate into the build output directory
 cd dist
-
-# if you are deploying to a custom domain
-# echo 'www.example.com' > CNAME
 
 git init
 git add -A
 git commit -m 'deploy'
 
-# if you are deploying to https://<USERNAME>.github.io/<REPO>
 git push -f git@github.com:timvanoostrom/gleisplanner.git master:gh-pages
 
 cd -
